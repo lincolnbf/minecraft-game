c39f42cb302121ec1b6d33179783ed16b54b0dbb
Your Name <you@example.com>
2022-06-06 13:17:27 -0300

    Added restart game funcionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const score = document.querySelector(".score");
 const CRASH_VALUE = 100;
 const ACTOR_JUMP_IFRAME = 100;
 
+let score_value = 0;
+let game_is_running = true;
+
 const jump = () => {
   actor.classList.add("jump");
 
@@ -12,30 +15,46 @@ const jump = () => {
   }, 1000);
 };
 
-let score_value = 0;
+const updateScore = () => {
+  score_value += 1;
+  score.innerHTML = score_value;
+};
 
-const loop = setInterval(() => {
+const clearAnimations = () => {
+  zombie.style.animation = "none";
+  zombie.style.left = CRASH_VALUE + "px";
+
+  actor.style.animation = "none";
+  actor.style.left = "0px";
+  actor.src = "assets/dead.png";
+};
+
+const die = () => {
+  game_is_running = false;
+};
+
+const run = setInterval(() => {
   const zombiePosition = zombie.offsetLeft;
   const actorBottomOffsetPosition = +window
     .getComputedStyle(actor)
     .bottom.replace("px", "");
 
-  score_value += 1;
-  score.innerHTML = score_value;
+  updateScore();
 
   if (
     zombiePosition <= CRASH_VALUE &&
     actorBottomOffsetPosition <= ACTOR_JUMP_IFRAME
   ) {
-    zombie.style.animation = "none";
-    zombie.style.left = CRASH_VALUE + "px";
-
-    actor.style.animation = "none";
-    actor.style.left = "0px";
-    actor.src = "assets/dead.png";
-
-    clearInterval(loop);
+    die();
+    clearAnimations();
+    clearInterval(run);
   }
 }, 100);
 
-document.addEventListener("keydown", jump);
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space" && game_is_running) {
+    jump();
+  } else if (e.code === "Space" && !game_is_running) {
+    location.reload();
+  }
+});
